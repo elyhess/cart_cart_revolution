4617cf3ccec91590b95326d36845f5e87fe9bc2e
Ely Hess <ely.hess@me.com>
2020-10-14 13:17:02 -0600

    Initial commit

---
+++ test/shopping_cart_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/product'
+require './lib/shopping_cart'
+class ShoppingCartTest < Minitest::Test
+
+  def test_it_exists_and_has_attributes
+    
+  end
+end
